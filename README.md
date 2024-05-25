dce248355e469c10ca3dc19247ae086b75f5ff45
Julian Harris <julian.harris@gmail.com>
2024-05-25 13:12:16 +0100

    initial build extracting prices from Claude's html page

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# llm-price-scraper-go
-Creates a pricing.json file containing pricing information for various LLMs
+# Failed experiment
+
+The idea here was to have a really simple automatic way to compare pricing of different systems.
+
+But there is no API for much of this and scraping is… involved. 
+
+Scraping content from modern web sites like openai's requires bypassing its human checking which is more work than I can be bothered with for now. 
+
--- main.go
+++ main.go
@@ -7,9 +7,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"time"
 
-	"github.com/chromedp/chromedp"
-	claude "github.com/potproject/claude-sdk-go"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/option"
 )
 
 type TokenPrice struct {
@@ -27,116 +30,106 @@ type PriceScraperResponse struct {
 	ModelPrices []ModelPrice `json:"modelPrices"`
 }
 
-const DEFAULT_CLAUDE_MODEL = "claude-3-haiku-20240307"
+func ExtractMainContent(htmlContent string) (string, error) {
+	// Create a new document from the HTML string
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlContent))
+	if err != nil {
+		return "", fmt.Errorf("error creating document from HTML: %w", err)
+	}
+
+	// Find the <main> tag and extract its contents
+	mainContent, err := doc.Find("main").Html()
+	if err != nil {
+		return "", fmt.Errorf("error extracting <main> content: %w", err)
+	}
+
+	return mainContent, nil
+}
 
 func main() {
 	ctx := context.Background()
 
-	url := flag.String("openai", "https://openai.com/api/pricing/", "URL to fetch HTML from")
+	// Set default URL to local file path
+	localHtml := flag.String("local-html", "data/claude.html", "Path html page containing Claude pricing")
+	scraperModel := flag.String("scraper-model", "gemini-1.5-flash-latest", "Gemini model used for scraping prices")
 	flag.Parse()
 
-	htmlContent, err := fetchHTML(*url)
+	htmlContent, err := getHTML(*localHtml)
 	if err != nil {
 		log.Fatalf("Error fetching HTML: %v", err)
 	}
 
-	fmt.Printf("HTML: %s\n", htmlContent)
+	client, model := newGeminiClientModel(ctx, *scraperModel)
+	defer client.Close()
+	if err != nil {
+		log.Fatalf("Error creating Gemini client: %v", err)
+	}
 
-	client := newClaudeClient()
-
-	systemPrompt := generateSystemPrompt()
-	fmt.Printf("System Prompt: %s\n", systemPrompt)
-	priceData, err := extractPrices(ctx, client, systemPrompt, htmlContent)
+	start := time.Now()
+	priceData, resp, err := extractPrices(ctx, model, htmlContent)
 	if err != nil {
 		log.Fatalf("Error extracting prices: %v", err)
 	}
+	elapsed := time.Since(start)
 
 	fmt.Printf("Extracted Prices: %+v\n", priceData)
+	fmt.Printf("\nTokens generated: %d\n", resp.UsageMetadata.CandidatesTokenCount)
+	fmt.Printf("Input token count: %d\n", resp.UsageMetadata.PromptTokenCount)
+	fmt.Printf("Output tokens per Second: %.2f\n", float64(resp.UsageMetadata.CandidatesTokenCount)/elapsed.Seconds())
+	fmt.Printf("Total Execution Time: %s\n", elapsed)
+
 }
 
-func newClaudeClient() *claude.Client {
-	apiKey, exists := os.LookupEnv("ANTHROPIC_API_KEY")
-	if !exists {
-		log.Fatal("ANTHROPIC_API_KEY environment variable is not set")
+func newGeminiClientModel(ctx context.Context, modelString string) (*genai.Client, *genai.GenerativeModel) {
+	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("GEMINI_API_KEY")))
+	if err != nil {
+		log.Fatal(err)
 	}
-	client := claude.NewClient(apiKey)
-	return client
+
+	model := client.GenerativeModel(modelString)
+
+	return client, model
 }
 
-// func fetchHTML(url string) (string, error) {
-// 	resp, err := http.Get(url)
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	defer resp.Body.Close()
-
-// 	body, err := io.ReadAll(resp.Body)
-// 	if err != nil {
-// 		return "", err
-// 	}
-
-// 	return string(body), nil
-// }
-
-func fetchHTML(url string) (string, error) {
-	// Create context
-	ctx, cancel := chromedp.NewContext(context.Background())
-	defer cancel()
-
-	var htmlContent string
-
-	// Run task
-	err := chromedp.Run(ctx,
-		chromedp.Navigate(url),
-		chromedp.WaitVisible(`body`, chromedp.ByQuery),
-		chromedp.OuterHTML("html", &htmlContent),
-	)
+func getHTML(filePath string) (string, error) {
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+	mainContent, err := ExtractMainContent(string(content))
 	if err != nil {
 		return "", err
 	}
 
-	return htmlContent, nil
+	return string(mainContent), nil
 }
 
-func extractPrices(ctx context.Context, client *claude.Client, prompt, htmlContent string) (*PriceScraperResponse, error) {
-	req := claude.RequestBodyMessages{
-		Model:       "claude-3-haiku-20240307",
-		MaxTokens:   1000,
-		System:      prompt,
-		Temperature: 0.0,
-		Messages: []claude.RequestBodyMessagesMessages{
-			{
-				Role:    claude.MessagesRoleUser,
-				Content: htmlContent,
-			},
-		},
-	}
+func extractPrices(ctx context.Context, model *genai.GenerativeModel, htmlContent string) (*PriceScraperResponse, *genai.GenerateContentResponse, error) {
+	systemPrompt := generateTypedHtmlScraperSystemPrompt()
+	fmt.Printf("System Prompt: %s\n", systemPrompt)
 
-	resp, err := client.CreateMessages(ctx, req)
+	resp, err := model.GenerateContent(ctx, genai.Text(systemPrompt+htmlContent))
 	if err != nil {
-		return nil, err
+		log.Fatal(err)
 	}
 
-	if len(resp.Content) == 0 {
-		return nil, fmt.Errorf("no response from Claude")
-	}
-
-	content := resp.Content[0].Text
-	fmt.Printf("Received: \n%s\n", content)
+	// Parts can be Text, FunctionResponse or a Blob but we know it's Text in this case so the cast is good
+	content := string(resp.Candidates[0].Content.Parts[0].(genai.Text))
+	fmt.Printf("--- Received \n%s\n---", content)
 	var decodedMessage PriceScraperResponse
 	err = json.Unmarshal([]byte(content), &decodedMessage)
 	if err != nil {
-		return nil, fmt.Errorf("JSON decoding error: %v", err)
+		return nil, resp, fmt.Errorf("JSON decoding error: %v", err)
 	}
 
-	return &decodedMessage, nil
+	return &decodedMessage, resp, nil
 }
 
-func generateSystemPrompt() string {
+func generateTypedHtmlScraperSystemPrompt() string {
 	exampleResponse := PriceScraperResponse{
 		ModelPrices: []ModelPrice{
 			{
-				ModelName: "GPT-3.5",
+				ModelName: "gpt-3.5-turbo",
 				InputTokenPrice: TokenPrice{
 					CostPerMillion: 0.01,
 					Currency:       "USD",
@@ -147,14 +140,14 @@ func generateSystemPrompt() string {
 				},
 			},
 			{
-				ModelName: "GPT-4",
+				ModelName: "gpt-4-32k",
 				InputTokenPrice: TokenPrice{
 					CostPerMillion: 0.03,
-					Currency:       "EUR",
+					Currency:       "USD",
 				},
 				OutputTokenPrice: TokenPrice{
 					CostPerMillion: 0.04,
-					Currency:       "EUR",
+					Currency:       "USD",
 				},
 			},
 		},
@@ -165,5 +158,5 @@ func generateSystemPrompt() string {
 		log.Fatalf("Error marshaling example response: %v", err)
 	}
 
-	return fmt.Sprintf("You are a price extraction API that takes public data from HTML and extracts data formatted using the PriceScraperResponse schema, exemplified below:\n%s", string(jsonExample))
+	return fmt.Sprintf("You are a price extraction API that takes public data from HTML and extracts data, returning it EXACTLY using the PriceScraperResponse schema so it can be read by a JSON parser, with no escape quotes before or after, exemplified as:\n%s", string(jsonExample))
 }
